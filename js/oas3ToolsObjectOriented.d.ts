2983dd7ec77bd39e5981669216d740ed7da423ce
Serhii Temchenko <serg.temchenko@gmail.com>
2021-08-04 11:30:50 +0200

    gitpkg

---
+++ js/oas3ToolsObjectOriented.d.ts
@@ -0,0 +1,2 @@
+import { Oas3ToolsObjectOrientedConfig } from './interfaces';
+export declare const oas3ToolsObjectOriented: (controllers: object[], configuration: Oas3ToolsObjectOrientedConfig) => import("express").Application;
