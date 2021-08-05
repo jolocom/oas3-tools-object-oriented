4da637faa8146c5bbfb145048477d6fc2ddf9089
Serhii Temchenko <serg.temchenko@gmail.com>
2021-08-05 17:21:34 +0200

    docs: added docblock to the main function

--- src/oas3ToolsObjectOriented.ts
+++ src/oas3ToolsObjectOriented.ts
@@ -17,6 +17,14 @@ const createOas3ToControllerMethodMap = (controllers: Record<string, any>[]): Na
   }).reduce((maps, mapItem) => ({ ...maps, [mapItem.path]: mapItem.handler }), {})
 }
 
+/**
+ * The wrapper over 'oas3-tools' which helps to wire up 'OpenAPI Specification' declaration with controllers classes methods
+ *
+ * @param controllers Collection of the controllers instances
+ * @param configuration The 'oas3-tools' configuration
+ *
+ * @return express.Application Configured by 'oas3-tools' express application
+ */
 export const oas3ToolsObjectOriented = (controllers: object[], configuration: Oas3ToolsObjectOrientedConfig) => {
   const oas3ToControllerMethodMap = createOas3ToControllerMethodMap(controllers)
   const oas3AppConfig = oas3Tools.expressAppConfig(
